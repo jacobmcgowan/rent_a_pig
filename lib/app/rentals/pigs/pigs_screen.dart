f99aafa2bfcc733658540ac397c3bbf3611076e5
Jacob McGowan <jacob.mcgowan@rovisys.com>
2019-06-06 12:51:37 -0400

    Cleaned up the pigs screen.

--- lib/app/rentals/pigs/pigs_screen.dart
+++ lib/app/rentals/pigs/pigs_screen.dart
@@ -4,60 +4,44 @@ import 'package:rent_a_pig/shared/models.dart';
 import 'package:rent_a_pig/shared/services.dart';
 
 /// Displays the list of pigs that are available for a job.
-class PigsScreen extends StatefulWidget {
+class PigsScreen extends StatelessWidget {
   /// The job to find pigs for.
   final String job;
+  final String _title;
+  final List<Pig> _pigs;
+
+  // Classes that inherit from a widget should always be immutable. The
+  // following constructor show an example of how to instantiate final fields
+  // using some logic. 
 
   /// Creates a PigsScreen.
-  PigsScreen({Key key, this.job}) : super(key: key);
-
-
-  @override
-  _PigsScreenState createState() {
-    return new _PigsScreenState(
-      job: job,
-      title: job == null ? 'All Pigs' : 'Pigs for ' + job,
-      pigs: PigsService.getPigs(job)
-    );
-  }
-}
-
-
-/// Handles the logic and internal state of a PigsScreen.
-class _PigsScreenState extends State<PigsScreen> {
-  /// The job to find pigs for.
-  final String job;
-
-  /// The title to display in the AppBar.
-  final String title;
-
-  /// The list of pigs to display.
-  final List<Pig> pigs;
-
-  /// Creates a _PigsScreenState.
-  _PigsScreenState({this.job, this.title, this.pigs});
-
+  PigsScreen({Key key, this.job}) :
+    _title = job == null ? 'All Pigs' : 'Pigs for ' + job,
+    _pigs = PigsService.getPigs(job),
+    super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(_title),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Center(
-          child: _buildPigsList(),
+          child: _buildPigsList(context),
         ),
       ),
     );
   }
 
+
   /// Builds the pigs list.
-  Widget _buildPigsList() {
-    Widget pigSummaries = pigs.length > 0 ?
+  Widget _buildPigsList(BuildContext context) {
+    Widget pigSummaries = _pigs.length > 0 ?
       Column(
-        children: pigs
+        children: _pigs
           .map((pig) => PigSummary(pig: pig,))
           .toList()
         ) :
